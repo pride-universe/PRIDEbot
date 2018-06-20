13f40c38922869c6e68dd1c63c04926751c4e144
MrOpposite <admin@opposite.biz>
2018-06-20 18:43:33 +0200

    Added WolframAlpha command

---
+++ commands/util/wa.js
@@ -0,0 +1,41 @@
+const commando = require('discord.js-commando');
+const { waSecret } = require('../../secrets');
+const { stripIndents, oneLine } = require('common-tags');
+const wolfram = require('wolfram-alpha').createClient(waSecret);
+
+module.exports = class WaCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'wa',
+      aliases: ['wolfram', 'wolframalpha'],
+      group: 'util',
+      memberName: 'wa',
+      description: 'Does math calculations using Wolfram Alpha',
+      examples: ['wa distance from the sun to earth', 'wa 1 liter in oz'],
+      guildOnly: false,
+      clientPermissions: [],
+      format: '<query>',
+    });
+  }
+  async run(msg, args) {
+    msg.channel.startTyping();
+    let response;
+    try {
+      response = await wolfram.query(args);
+    } catch (e) {
+      msg.channel.stopTyping();
+      msg.reply("Error whith connection to WolframAlpha.");
+      return;
+    }
+    response = response.find(e=>e.primary);
+    console.log(response);
+    if(!response) {
+      msg.reply("WolframAlpha did not respond with any result.");
+      return;
+    }
+    const title = response.title;
+    response = response.subpods.map(e=>e.text).join("\n\n");
+    msg.channel.stopTyping();
+    msg.reply(`**${title}:**\n${response}`);
+  }
+};
