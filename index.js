68271b0854fe0c24f73e2b78d91d3a38b17648e4
MrOpposite <admin@opposite.biz>
2018-06-27 00:54:50 +0200

    Disabled banned-joke

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('./extensions');
 const bot = require('./bot');
 require('./modules/triggerWarnings');
 require('./modules/spoilers');
-require('./jokes/banned');
+//require('./jokes/banned');
 require('./jokes/cultTracker');
 
 if(!process.argv.find((e)=>e==="--no-repl")) {
