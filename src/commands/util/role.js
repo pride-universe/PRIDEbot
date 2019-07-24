7daa58ce4c779841060d102a4671df7f70f42ca0
Linn Dahlgren <admin@opposite.biz>
2019-07-25 00:10:48 +0200

    Redid self-role system.

--- src/commands/util/role.js
+++ src/commands/util/role.js
@@ -1,5 +1,9 @@
 const RestrictedCommand = require('../../restrictedCommand');
 
+const startRegex = /^SELF-START\((\d+)\)\|([^|\n]+)(?:\|([^|\n]*))?$/;
+const endRegex = /^SELF-END\((\d+)\)$/;
+const invalidGroup = Symbol('invalidGroup');
+const { FriendlyError } = require('discord.js-commando');
 module.exports = class RoleCommand extends RestrictedCommand {
   constructor(client) {
     super(client, {
@@ -13,26 +17,55 @@ module.exports = class RoleCommand extends RestrictedCommand {
       format: '[role[~<number>]]',
       permGroup: 'members',
     });
+    this.cache = new Map();
+    this.invalidateCache = (role) => this.cache.delete(role.guild.id);
+    this.startListeners();
   }
 
-  async listRoles(roles, msg) {
+  startListeners()  {
+    this.client.on('roleCreate', this.invalidateCache);
+    this.client.on('roleDelete', this.invalidateCache);
+    this.client.on('roleUpdate', this.invalidateCache);
+  }
+
+  stopListeners() {
+    this.client.off('roleCreate', this.invalidateCache);
+    this.client.off('roleDelete', this.invalidateCache);
+    this.client.off('roleUpdate', this.invalidateCache);
+  }
+
+  unload() {
+    this.stopListeners();
+    super.unload();
+  }
+  
+  reload() {
+    this.stopListeners();
+    super.reload();
+  }
+  
+  async listRoles(groups, msg) {
+
     const dupes = new Map();
     let str = 'List of available roles:\n';
-    let maxLen = roles.reduce((acc, role) => Math.max(acc, role.name.length), 0);
     let hasDupe = false;
-    for(const [,role] of roles) {
-      const dupeString = (()=>{
-        const dupe = dupes.get(role.name.toLowerCase());
-        if(!dupe) {
-          dupes.set(role.name.toLowerCase(), 2);
-          return '';
-        } else {
-          hasDupe = true;
-          dupes.set(role.name.toLowerCase(), dupe+1);
-          return ` [duplicate name, to select this role use \`\`${role.name}~${dupe}\`\`]`;
-        }
-      })();
-      str += `\`\`${role.name.padEnd(maxLen)}\u200b\`\`${msg.member.roles.has(role.id)?' `✅`':''}${dupeString}\n`;
+    for(const group of groups) {
+      const maxLen = group.roleEntries.reduce((acc, {role}) => Math.max(acc, role.name.length), 0);
+      str += `\n**${group.name}:\n**`
+      for(const {role} of group.roleEntries) {
+        const dupeString = (()=>{
+          const dupe = dupes.get(role.name.toLowerCase());
+          if(!dupe) {
+            dupes.set(role.name.toLowerCase(), 2);
+            return '';
+          } else {
+            hasDupe = true;
+            dupes.set(role.name.toLowerCase(), dupe+1);
+            return ` [duplicate name, to select this role use \`\`${role.name}~${dupe}\`\`]`;
+          }
+        })();
+        str += `\`\`${role.name.padEnd(maxLen)}\u200b\`\`${msg.member.roles.has(role.id)?' `✅`':''}${dupeString}\n`;
+      }
     }
     str += '\nRoles marked with `✅` are roles you already have\n';
     if(hasDupe) {
@@ -42,12 +75,14 @@ module.exports = class RoleCommand extends RestrictedCommand {
     msg.reply(str, {split: true});
   }
 
-  async assignRole(roles, msg, args) {
+  async assignRole(groups, permGroups, msg, args) {
     const dupes = new Map();
-    for(const [,role] of roles) {
-      let roleArr = dupes.get(role.name.toLowerCase());
-      if(!roleArr) dupes.set(role.name.toLowerCase(), roleArr = []);
-      roleArr.push(role);
+    for(const group of groups) {
+      for(const roleEntry of group.roleEntries) {
+        let roleArr = dupes.get(roleEntry.role.name.toLowerCase());
+        if(!roleArr) dupes.set(roleEntry.role.name.toLowerCase(), roleArr = []);
+        roleArr.push(roleEntry);
+      }
     }
     let [roleName, index] = args.split('~');
     roleName = roleName.trim();
@@ -60,7 +95,9 @@ module.exports = class RoleCommand extends RestrictedCommand {
     index--;
     const roleArr = dupes.get(roleName.toLowerCase());
     if(index >= roleArr.length) return msg.reply(`Can't find role \`\`${roleName}~${index+1}\`\``);
-    const role = roleArr[index];
+    const roleEntry = roleArr[index];
+    if(roleEntry.permGroup && !permGroups.has(roleEntry.permGroup)) return msg.reply(`You are not allowed to self-assign \`\`${roleName}${index?'~'+(index+1):''}\`\``);
+    const role = roleEntry.role;
     if(!msg.member.roles.has(role.id)) {
       msg.member.roles.add(role);
       return msg.reply(`Added role \`\`${role.name}\`\` to you`);
@@ -71,30 +108,116 @@ module.exports = class RoleCommand extends RestrictedCommand {
     
   }
 
+  getSelfRoleGroups(guild) {
+    if(this.cache.has(guild.id)) return this.cache.get(guild.id);
+    const groups = new Map();
+    guild.roles.forEach(r => {
+      let match;
+      if(match = r.name.match(startRegex)) {
+        const id = match[1];
+        const name = match[2];
+        const permGroup = match[3];
+        
+        if(!groups.has(id)) groups.set(id, {});
+        const group = groups.get(id);
+        if(group === invalidGroup) return;
+
+        if(group.start) {
+          groups.set(id, invalidGroup);
+          return;
+        }
+        group.name = name;
+        group.permGroup = permGroup;
+        group.start = r;
+        group.roleEntries = [];
+      } else if(match = r.name.match(endRegex)) {
+        const id = match[1];
+        
+        if(!groups.has(id)) groups.set(id, {});
+        const group = groups.get(id);
+        if(group === invalidGroup) return;
+
+        if(group.end) {
+          groups.set(id, invalidGroup);
+          return;
+        }
+
+        group.end = r;
+      }
+    });
+    for(let [key, group] of [...groups]) {
+      if(group !== invalidGroup &&
+        group.start &&
+        group.end &&
+        group.start.position > group.end.position)
+        continue;
+      
+      groups.delete(key);
+      this.client.emit('warn', `Self role group with id "${key}" is invalid.`)
+    }
+    const groupMarkers = new Set();
+    groups.forEach(({start, end}) => groupMarkers.add(start).add(end));
+    guild.roles.sort((r1,r2) => r2.position - r1.position).forEach(r => {
+      for(let [,group] of groups) {
+        if(r.position < group.start.position && r.position > group.end.position) {
+          if(groupMarkers.has(r)) throw new FriendlyError("Overlapping self-role groups. This is not good.");
+          if(r.managed) {
+            this.client.emit('warn', `Role '${r.name}' in '${guild.name}' is managed by external service and not assignable, omitting from self-role.`);
+            return;
+          }
+          if(r.permissions.bitfield & 0x79C0203E) {
+            this.client.emit('warn', `Role '${r.name}' in '${guild.name}' has dangerous permissions, omitting from self-role.`);
+            return;
+          }
+          const roleEntry = {
+            permGroup: group.permGroup,
+            role: r
+          };
+          group.roleEntries.push(roleEntry);
+        }
+      }
+    });
+
+    return [...groups].sort(([key1], [key2])=>key1-key2).map(([,group]) => group);
+  }
+
+  getPermGroups(msg) {
+    const permGroups = new Set();
+    const guildPerms = msg.guild.settings.get('permissionRoles') || {};
+    permLoop: for(const permGroupName of Object.keys(guildPerms)) {
+      const permGroup = guildPerms[permGroupName];
+      const roles = Array.isArray(permGroup)
+      ? permGroup
+      : [permGroup];
+      for(const role of roles) {
+        if(msg.member.roles.has(role)) {
+          permGroups.add(permGroupName);
+          continue permLoop;
+        }
+      }
+    }
+    return permGroups;
+  }
+
   async run(msg, args) {
     if(!msg.guild) return;
-    const guild = msg.guild;
-    const roleLimits = guild.settings.get('selfRoleLimits');
-    if(!roleLimits) return msg.reply('Server is not configured for self-roles');
-    if(!Array.isArray(roleLimits)) return msg.reply('Self-role configuration is malconfigured');
-    const [upper, lower] = roleLimits.map(role=>guild.roles.get(role)).filter(role=>role).sort((a,b)=>b.position-a.position);
-    if(upper === undefined || lower === undefined) return msg.reply('Self-role configuration is malconfigured');
-    const roles = guild.roles
-      .filter(role => role.position < upper.position && role.position > lower.position)
-      .filter(role => {
-        if(role.permissions.bitfield & 0x79C0203E) {
-          this.client.emit('warn', `Role '${role.name}' in '${guild.name}' has dangerous permissions, omitting from self-role.`);
-          return false;
-        }
-        return true;
-      })
-      .sort((a,b) => b.position - a.position);
+    const permGroups = this.getPermGroups(msg);
+
+    const groups = this.getSelfRoleGroups(msg.guild);
 
     if(!args) {
-      return this.listRoles(roles, msg);
+      const listGroups = groups.filter(group => {
+        return !group.permGroup || permGroups.has(group.permGroup);
+      });
+      if(!listGroups.length) {
+        return msg.reply(`BLA`)
+      }
+      return this.listRoles(listGroups, msg);
     } else {
+      const upper = [...groups.values()].reduce((acc, {start}) => acc ? acc.position < start.position ? start : acc : start, null);
+      console.log(upper.name, upper.position);
       if(msg.guild.me.roles.highest.position < upper.position) return msg.reply(`Missing role high enough to give out roles. I require role \`\`${upper.name}\`\` or higher to be able to hand out roles.`);
-      return this.assignRole(roles, msg, args);
+      return this.assignRole(groups, permGroups, msg, args);
     }
   }
 };
