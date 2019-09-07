9cdc8029197f8f6ca922280bce85085709754044
Linn Dahlgren <admin@opposite.biz>
2019-09-07 11:25:28 +0200

    Made newstatus work for members checking their own newmember status

--- src/commands/util/newstatus.js
+++ src/commands/util/newstatus.js
@@ -10,13 +10,13 @@ module.exports = class NewStatus extends RestrictedCommand {
     super(client, {
       name: 'newstatus',
       aliases: ['ns'],
-      group: 'staff',
+      group: 'util',
       memberName: 'newstatus',
       description: 'Get\'s the newmember status of a user',
-      examples: ['newstatus 141977677404962816', 'newstatus @Linn'],
+      examples: ['newstatus', 'newstatus 141977677404962816', 'newstatus @Linn'],
       clientPermissions: [],
-      format: '<discord id|mention>',
-      permGroup: 'staff',
+      format: '[discord id|mention]',
+      permGroup: 'members',
     });
   }
 
@@ -25,20 +25,33 @@ module.exports = class NewStatus extends RestrictedCommand {
     return members.filter(([,m])=>m.has(user.id)).map(([g,m])=>[g,m.get(user.id)]);
   }
 
+  pluralString(str, params) {
+    return str.replace(/{{([a-zA-Z]+)}}/g, (_,prop) => {
+      console.log(prop);
+      return `${params[prop].val} ${params[prop].label}${params[prop].val !== 1 ? 's' : ''}`
+    });
+  }
+
+  craftError(msg, args) {
+    return msg.reply(`${msg.author.id === args ? 'You do' : 'That does'} not seem to be someone with the new member role.`);
+  }
+
   async run(msg, args) {
     let member, newMemberData;
+    if(!args) args = msg.author.id;
     const match = args.match(/<@!?(\d+)>/);
     if(match) {
       args=match[1];
     }
+    if(!this.checkPermgroup(msg, true, 'staff') && args !== msg.author.id) return msg.reply('Only staff can check the status of other members');
     try {
       newMemberData = msg.guild.settings.get('newUsers')[args];
       member = await msg.guild.members.fetch(args);
     } catch (_) {
-      return msg.reply('That does not seem to be someone with the new member role.');
+      return this.craftError(msg, args);
     }
     if(!member || !newMemberData) {
-      return msg.reply('That does not seem to be someone with the new member role.');
+      return this.craftError(msg, args);
     }
     const embed = new MessageEmbed();
     embed.setAuthor(member.displayName, member.user.displayAvatarURL({size: 128}));
@@ -49,7 +62,20 @@ module.exports = class NewStatus extends RestrictedCommand {
     const daysAgo = Math.floor(msAgo / MS_IN_A_DAY);
     const hoursAgo = Math.floor((msAgo % MS_IN_A_DAY) / MS_IN_AN_HOUR);
     const minutesAgo = Math.floor(((msAgo % MS_IN_A_DAY) % MS_IN_AN_HOUR) / MS_IN_A_MINUTE);
-    embed.addField('Joined', `${daysAgo} days, ${hoursAgo} hours and ${minutesAgo} minutes ago`);
+    embed.addField('Joined', this.pluralString('{{days}}, {{hours}} and {{minutes}} ago', {
+      days: {
+        val: daysAgo,
+        label: 'day',
+      },
+      hours: {
+        val: hoursAgo,
+        label: 'hour',
+      },
+      minutes: {
+        val: minutesAgo,
+        label: 'minute',
+      },
+    }));
     embed.addField('Message count', String(newMemberData.messages));
     embed.setTimestamp();
     return msg.replyEmbed(embed);
--- src/restrictedCommand.js
+++ src/restrictedCommand.js
@@ -9,24 +9,30 @@ module.exports = class RestrictedCommand extends commando.Command {
 
     this.permGroup = permGroup;
   }
-  
-  hasPermission(message, ownerOverride = true) {
-    const superRes = super.hasPermission(message, ownerOverride);
-    if(superRes !== true) return superRes;
+
+  checkPermgroup(message, ownerOverride = true, permGroup) {
+    permGroup = permGroup || this.permGroup;
     if(ownerOverride && this.client.isOwner(message.author)) return true;
 
     const perms = message.guild.settings.get('permissionRoles');
-    if(!perms) return `The \`${this.name}\` command can only be used by ${this.permGroup}.`;
-    if(!perms[this.permGroup]) return `The \`${this.name}\` command can only be used by ${this.permGroup}.`;
-    const roles = Array.isArray(perms[this.permGroup])
-      ? perms[this.permGroup]
-      : [perms[this.permGroup]];
+    if(!perms) return false;
+    if(!perms[permGroup]) return false;
+    const roles = Array.isArray(perms[permGroup])
+      ? perms[permGroup]
+      : [perms[permGroup]];
 
     for(const role of roles) {
       if(message.member.roles.has(role)) {
         return true;
       }
     }
+    return false;
+  }
+  
+  hasPermission(message, ownerOverride = true) {
+    const superRes = super.hasPermission(message, ownerOverride);
+    if(superRes !== true) return superRes;
+    if(this.checkPermgroup(message, ownerOverride)) return true;
     return `The \`${this.name}\` command can only be used by ${this.permGroup}.`;
   }
 };
